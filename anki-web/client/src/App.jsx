dfe3605e9c958a9b54498c821caff468b1cc6829
YohanGH <YohanGH@proton.me>
2024-07-14 14:56:32 +0200

    base repositories

--- anki-web/client/src/App.jsx
+++ anki-web/client/src/App.jsx
@@ -1,9 +1,24 @@
-import "./App.css";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
-      <h1>React</h1>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
--- anki-web/client/src/main.jsx
+++ anki-web/client/src/main.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
