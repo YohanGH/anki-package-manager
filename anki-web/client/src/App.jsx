1537f720fc52dc2a4de04e2c3fde14e1269c4f41
YohanGH <YohanGH@proton.me>
2024-07-15 15:40:04 +0200

    remove toast
    Signed-off-by: YohanGH <YohanGH@proton.me>

--- anki-web/client/src/App.jsx
+++ anki-web/client/src/App.jsx
@@ -1,21 +1,9 @@
 import { Outlet } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
+      <my>App</my>
       <main>
         <Outlet />
       </main>
