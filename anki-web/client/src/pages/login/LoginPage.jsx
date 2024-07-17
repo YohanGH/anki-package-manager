d8e01e5999c0e5bd83d4a4a7f1685c4a3dc9545b
YohanGH <YohanGH@proton.me>
2024-07-17 14:56:18 +0200

    create page login
    Signed-off-by: YohanGH <YohanGH@proton.me>

---
+++ anki-web/client/src/pages/login/LoginPage.jsx
@@ -0,0 +1,106 @@
+import { Checkbox, Box, CssBaseline, FormControlLabel, Grid, TextField, Typography, Button, Link, Avatar, Paper } from "@mui/material";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+// import components 
+import Copyright from "../../components/copyright/Copyright";
+
+function LoginPage() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // TODO : Modification le console.info
+        console.info({
+            email: data.get('email'),
+            password: data.get('password'),
+        })
+    }
+
+    return (
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              'url("/static/images/templates/templates-images/sign-in-side-bg.png")', // TODO : Change image
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "left",
+          }}
+        />{" "}
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: "8rem",
+              mx: "4rem",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: "1", bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Connexion
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: "1rem" }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: "3rem", mb: "2rem" }}
+              >
+                Connexion
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="/" variant="body2" >
+                    Forgot password ?
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    );
+}
+
+export default LoginPage;
