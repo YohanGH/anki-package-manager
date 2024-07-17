4ce1590eda6552b9eba9c4023c2bb4ef92682fa1
YohanGH <YohanGH@proton.me>
2024-07-17 09:27:24 +0200

    create navbar
    Signed-off-by: YohanGH <YohanGH@proton.me>

---
+++ anki-web/client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
+import { Link as RouterLink } from "react-Router-dom";
+
+function NavBar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Logo
+        </Typography>
+        <Box>
+          <Button color="inherit" component={RouterLink} to="/">
+            Accueil
+          </Button>
+          <Button color="inherit" component={RouterLink} to="create-deck">
+            Créer un Deck
+          </Button>
+          <Button color="inherit" component={RouterLink} to="manage-decks">
+            Gérer les Decks
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default NavBar;
