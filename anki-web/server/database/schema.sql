d920a1363077ec8bec2d57cdbe0f82d98914f726
YohanGH <YohanGH@proton.me>
2024-07-17 09:16:19 +0200

    Create schema tables

---
+++ anki-web/server/database/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    role VARCHAR(50) NOT NULL,
+    email VARCHAR(124) NOT NULL,
+    password VARCHAR(150) NOT NULL,
+);
+
+CREATE TABLE categorie (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE deck (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL,
+    id_categorie INT,
+    id_user INT,
+    FOREIGN KEY (id_categorie) REFERENCES categorie(id),
+    FOREIGN KEY (id_user) REFERENCES user(id)
+);
+
+CREATE TABLE feedback (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    content TEXT NOT NULL,
+    id_deck INT,
+    FOREIGN KEY (id_deck) REFERENCES deck(id)
+);
