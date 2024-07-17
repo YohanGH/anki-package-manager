8d3f8bea0abc429f62f272edeb030f1a6c53b2a2
YohanGH <YohanGH@proton.me>
2024-07-17 13:58:11 +0200

    Create route deck management
    Signed-off-by: YohanGH <YohanGH@proton.me>

--- anki-web/client/src/main.jsx
+++ anki-web/client/src/main.jsx
@@ -7,6 +7,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import HomePage from "./pages/home_page/HomePage";
 import DeckCreation from "./pages/deck_creation/DeckCreation";
+import DeckManagement from "./pages/deck_management/DeckManagement";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
       {
         path: "create-deck",
         element: <DeckCreation />,
+      },
+      {
+        path: "manage-decks",
+        element: <DeckManagement />
       },
     ],
   },
