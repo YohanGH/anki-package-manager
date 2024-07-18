ba22d68fe9a4fc08d1b7d131776ae5a6aab7be70
YohanGH <YohanGH@proton.me>
2024-07-18 11:59:09 +0200

    create controller for categorie, categorieRepository and in table js link for tables
    Signed-off-by: YohanGH <YohanGH@proton.me>

--- anki-web/server/database/tables.js
+++ anki-web/server/database/tables.js
@@ -1,5 +1,6 @@
 // Import the repository modules responsible for handling data operations on the tables
 const DeckRepository = require("./models/DeckRepository"); 
+const CategorieRepository = require("./models/CategorieRepository");
 
 // Create an empty object to hold data repositories for different tables
 const tables = {};
@@ -11,6 +12,7 @@ const tables = {};
 
 // Register each repository as data access point for its table
 tables.deck = new DeckRepository();
+tables.categorie = new CategorieRepository();
 
 /* ************************************************************************* */
 
