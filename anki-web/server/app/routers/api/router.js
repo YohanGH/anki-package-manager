c4878d249b1008afc305d56df157513c706a71ae
YohanGH <YohanGH@proton.me>
2024-07-18 11:58:02 +0200

    Create router for categorie
    Signed-off-by: YohanGH <YohanGH@proton.me>

--- anki-web/server/app/routers/api/router.js
+++ anki-web/server/app/routers/api/router.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 // Import And Use Routers Here
 const deckRouter = require("./deck/router");
+const categorieRouter = require("./categorie/router")
 
 // The main router uses sub-routers for different resources
 // Exemple de route
 router.use("/deck", deckRouter);
+router.use("/categorie", categorieRouter);
 
 module.exports = router;
